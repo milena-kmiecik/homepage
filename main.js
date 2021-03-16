141096a378ca0755070b276d63c7b45b6bbe5f2e
Milena Kmiecik <kmiecik.milena8@gmail.com>
2021-03-16 19:30:22 +0100

    DOM&javascript

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-alert('siema!');
+const firstName = `Milena`;
+const age = 29;
+console.log(`Witaj Drogi Odwiedzający, nazywam się ${firstName} i mam ${age} lat.`
+);
+const heading = document.querySelector('.main-heading--js');
+heading.innerHTML = `Witaj Drogi Odwiedzający, nazywam się ${firstName} i mam ${age} lat.`;
+const emptyParagraph = document.querySelector('.article__descripion--js');
+emptyParagraph.innerHTML = `Nawet uzupełniłam treść za pomocą javascriptu!`
