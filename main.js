796c0be517142d6de3bfb1cd93e5cf5196e92862
Milena Kmiecik <kmiecik.milena8@gmail.com>
2021-04-14 20:26:15 +0200

    background_gradient_display_flex_on_navigation_normalize_css

--- main.js
+++ main.js
@@ -6,3 +6,30 @@ const heading = document.querySelector('.main-heading--js');
 heading.innerHTML = `Witaj Drogi Odwiedzający, nazywam się ${firstName} i mam ${age} lat.`;
 const emptyParagraph = document.querySelector('.article__descripion--js');
 emptyParagraph.innerHTML = `Nawet uzupełniłam treść za pomocą javascriptu!`
+let myNumber = 5
+function calculate (myNumber) {
+    console.log(`Dostałam ${myNumber}`);
+    return myNumber*7;
+}
+const myResult = calculate (1);
+console.log(myResult);
+const myResult2 = calculate (5);
+console.log(myResult2);
+function greet(age,firstName){
+    console.log(
+        `Witaj drogi Odwiedzający, jestem ${firstName} i mam ${age} lat! Miło mi Cię gościć na mojej stronie`
+    );
+}
+greet(age,firstName)
+
+function createContent(querySelectorContent, content) {
+    const element = document.querySelector(querySelectorContent)
+    element.innerHTML = content;
+}
+createContent('.article__descripion--js', 'Witaj świecie!');
+
+function helloWorld() {
+    console.log('Witaj świecie!')
+}
+helloWorld();
+createContent('.article__title--js', 'Podmieniony tytuł!');
